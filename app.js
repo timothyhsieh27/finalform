bb438c98a91a201687b3df48c937ed3226096006
Tim <timothyhsieh27@gmail.com>
2016-10-14 15:11:21 -0400

    Fixed showStatus message to only display failure or success at one time.

--- app.js
+++ app.js
@@ -3,22 +3,36 @@ var display = $('#display')
 var base = 0
 var clickLine = $("<p></p>")
 var statusLine = $("<p></p>")
+var goal = 5
+// var goal = $('.goal').val()
 
 trainButton.click(function() {
-  appendCountertext()
-  showStatustext()
+  var counter = (base += 1)
+  showStatustext(counter)
+  appendCountertext(counter)
 })
 
-function appendCountertext() {
-
-  var counter = (base += 1)
+function appendCountertext(counter) {
   var countertext = clickLine.text("You have clicked " + counter + " times.")
   display.append(countertext)
   console.log("You have clicked " + counter + " times.")
-
 }
 
-function showStatustext() {
-  var statustext = statusLine.text("You have not yet reached your final form.")
-  display.append(statustext)
+function showStatustext(counter) {
+  if(counter >= goal) {
+    showWin(counter)
+  }
+  else {
+    showFail(counter)
+  }
+}
+
+function showWin(counter) {
+  var wintext = statusLine.text("THIS IS MY FINAL FORM")
+  display.append(wintext)
+}
+
+function showFail(counter) {
+  var failtext = statusLine.text("This isn't even my final form.")
+  display.append(failtext)
 }

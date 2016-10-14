705d944d4fadb03a6de12253fa26f499cfc960f6
Tim <timothyhsieh27@gmail.com>
2016-10-14 12:29:41 -0400

    Functionality added - now displays status text for failing.

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 var trainButton = $('.train')
 var display = $('#display')
 var base = 0
-var paragraph = $("<p></p>")
+var clickLine = $("<p></p>")
+var statusLine = $("<p></p>")
 
 trainButton.click(function() {
-  var counter = base += 1
-  var countertext = paragraph.text("You have clicked " + counter + " times.")
+  appendCountertext()
+  showStatustext()
+})
+
+function appendCountertext() {
+
+  var counter = (base += 1)
+  var countertext = clickLine.text("You have clicked " + counter + " times.")
   display.append(countertext)
   console.log("You have clicked " + counter + " times.")
-})
+
+}
+
+function showStatustext() {
+  var statustext = statusLine.text("You have not yet reached your final form.")
+  display.append(statustext)
+}
